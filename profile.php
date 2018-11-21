48bbc9afcb1ade629d62a02c6b88a034790b07b0
Muganda Imo <imo.muganda21@gmail.com>
2018-11-21 16:46:25 +0300

    Ideas functions in progress

--- profile.php
+++ profile.php
@@ -3,6 +3,10 @@
 include 'core/init.php';
 protect(); //Checks if the user is logged in.
 include "pages/includes/header.php";
+
+$ind_id = $user_data['industry_id'];
+$industry = mysqli_fetch_assoc(mysqli_query($conn,"select industry_name from industries where industry_id = $ind_id"))['industry_name'];
+
 ?>
 
 <h1><?php echo $user_data['firstname'].' '.$user_data['lastname'];?></h1>
@@ -11,6 +15,8 @@ include "pages/includes/header.php";
 
 Username: <?php echo $user_data['username'];?><br><br>
 Email: <?php echo $user_data['email'];?><br><br>
+Industry: <?php echo $industry;?><br><br>
+Logged in as: <?php echo ($user_data['type'] != 2) ? 'Industry':'Academia';?><br><br>
 
 
 		
